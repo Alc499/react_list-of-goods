4744caa5e149db430e234c39e98f08022883f56f
Alc499 <Ali.vitchenko@gmail.com>
2019-07-10 13:28:07 +0300

    Updates

---
+++ precache-manifest.f0477e0c99035f8b4ffc6fac3fdb35ea.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecc274a221c7f087f530dc6aec7d9d21",
+    "url": "/react_list-of-goods/index.html"
+  },
+  {
+    "revision": "1744db918d7799a6e6d6",
+    "url": "/react_list-of-goods/static/js/2.164617b5.chunk.js"
+  },
+  {
+    "revision": "0b20ccd156a4bdac6d20",
+    "url": "/react_list-of-goods/static/js/main.a7362364.chunk.js"
+  },
+  {
+    "revision": "e82fdff4b55e53116e45",
+    "url": "/react_list-of-goods/static/js/runtime~main.14fd7ef8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_list-of-goods/precache-manifest.d1a60a28eda3e5f773fa801cba116629.js"
+  "/react_list-of-goods/precache-manifest.f0477e0c99035f8b4ffc6fac3fdb35ea.js"
 );
 
 self.addEventListener('message', (event) => {
